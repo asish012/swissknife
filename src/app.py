684759d15e98aebdc3f5c4743e6224d9b08dbc51
asish012 <asish012@gmail.com>
2024-04-26 12:14:34 +0200

    wide page width

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 
+# Application Init
+st.set_page_config(layout="wide")
+
+# Sidebar Menu
 def menu():
     st.sidebar.page_link("app.py", label="Home")
     st.sidebar.page_link("pages/audio.py", label="Audio")
